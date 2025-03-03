c1e72266c0c3600b15d3061272049e26c36af376
Tony Aldea <tonythecoder@outlook.com>
2025-03-03 18:59:30 -0500

    Add SharedStateService for cart management
    Implement SharedStateService to track and update cart count across components. Update Cart.razor, NavBar.razor, and Home.razor to utilize the service for real-time cart updates. Modify IShoppingCartRepository to include GetTotalCartCartCountAsync method, and implement it in ShoppingCartRepository. Register SharedStateService as a singleton in Program.cs to ensure consistent state management.

---
+++ Repository/Services/SharedStateService.cs
@@ -0,0 +1,22 @@
+﻿namespace YumBlazor.Services
+{
+    //This class manages and notifies the state of the cart count
+    public class SharedStateService
+    {
+        public event Action OnChange;
+        private int _totalCartCount;
+
+        public int TotalCartCount
+        {
+            get => _totalCartCount;
+            set
+            {
+                _totalCartCount = value;
+               NotifyStateChanged();
+            }
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+    }
+}
