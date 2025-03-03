c147016d7c52c80f268e0acbcb1fc1ffcb6d4349
Tony Aldea <tonythecoder@outlook.com>
2025-03-02 20:08:32 -0500

    Refactor IOrderRepository and implement OrderRepository
    Updated namespace in IOrderRepository and standardized method parameter names. Created OrderRepository to implement IOrderRepository with methods that currently throw NotImplementedException.

--- Repository/IRepository/IOrderRepository.cs
+++ Repository/IRepository/IOrderRepository.cs
@@ -1,12 +1,13 @@
 ﻿using YumBlazor.Data;
 
-namespace YumBlazor.Repository.IOrderRepository
+namespace YumBlazor.Repository.IRepository
 {
     public interface IOrderRepository
     {
-        public Task<OrderHeader> CreateAsync(OrderHeader orderHeader);
-        public Task<OrderHeader> GetAsync(int id);
-        public Task<IEnumerable<OrderHeader>> GetAllAsync(string? userID=null);
-        public Task<OrderHeader> UpdateStatusAsync(int orderID, string status);
+
+        public Task<OrderHeader> CreateAsync(OrderHeader orderheader);
+        public Task<OrderHeader> GetAsync(int orderId);
+        public Task<IEnumerable<OrderHeader>> GetAllAsync(string? userid=null);
+        public Task<OrderHeader> UpdateStatusAsync(int orderId, string status);
     }
 }
---
+++ Repository/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using YumBlazor.Data;
+using YumBlazor.Repository.IRepository;
+
+namespace YumBlazor.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public Task<OrderHeader> CreateAsync(OrderHeader orderheader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<OrderHeader>> GetAllAsync(string? userid = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderHeader> GetAsync(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderHeader> UpdateStatusAsync(int orderId, string status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
