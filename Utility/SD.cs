9667db00760b4eb88f292c633b12b857594d0fed
Tony Aldea <tonythecoder@outlook.com>
2025-03-02 19:32:34 -0500

    Add order status management and repository interface
    - Set `OrderHeader.Status` to `SD.StatusPending` in `Cart.razor`.
    - Create `IOrderRepository` interface for order operations.
    - Introduce static properties for order statuses in `SD.cs`.

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -1,8 +1,16 @@
 ﻿namespace YumBlazor.Utility
+//This is a static class that will hold all the static details
+//of the application.For example, the roles of the users, the session keys, etc.
+//This class will be used to avoid hardcoding the values in the application.
 {
     public class SD
     {
         public static string Role_Admin = "Admin";
         public static string Role_Customer = "Customer";
+
+        public static string StatusPending = "Pending";
+        public static string StatusReadyForPickup = "ReadyForPickup";
+        public string StatusCompleted = "Completed";
+        public static string StatusCancelled = "Cancelled";
     }
 }
